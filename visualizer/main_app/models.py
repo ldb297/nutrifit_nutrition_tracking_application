6adda46540983a7bcae08677d9a039eb2775823b
Leonidas <ldbrooks297@gmail.com>
2021-04-01 15:06:44 -0700

    models.py: fix associations for cleaner data calls

--- visualizer/main_app/models.py
+++ visualizer/main_app/models.py
@@ -3,23 +3,11 @@ from datetime import date
 from django.contrib.auth.models import User
 
 MEALS = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner')
+    ('B', 'Breakfast'), 
+    ('L', 'Lunch'), 
+    ('D', 'Dinner'),
+    ('S', 'Snack')
 )
-
-class Meal(models.Model):
-    date = models.DateField('when you ate')
-    meal = models.CharField(
-        max_length=1,
-        choices= MEALS,
-        default= MEALS[0][0]
-    )
-    
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.user} had {self.meal} on {self.date}"
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, default='None')
@@ -28,7 +16,19 @@ class Ingredient(models.Model):
     fats = models.IntegerField()
     sugars = models.IntegerField()
 
-    meal = models.ManyToManyField(Meal)
-
     def __str__(self):
         return self.name
+
+class Meal(models.Model):
+    date = models.DateField('when you ate')
+    meal = models.CharField(
+        max_length=10,
+        choices= MEALS,
+        default= MEALS[0][0]
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return f"{self.user} had {self.meal} on {self.date}"
