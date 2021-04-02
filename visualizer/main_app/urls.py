8f66125a6ebbc1efb8399b88f44de54a0da35c50
Leonidas <ldbrooks297@gmail.com>
2021-04-01 17:10:04 -0700

    create urls routes

---
+++ visualizer/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('meals/', views.meals_index, name='meals'),
+    path('meals/<int:meal_id>/', views.meals_show, name='meals_show'),
+    path('meals/create/', views.meals_new, name='meals_create'),
+    path('meals/<int:pk>/update/', views.meals_update, name='meals_update'),
+    path('meals/<int:pk>/delete/', views.MealDelete.as_view(), name='meals_delete'),
+    path('meals/<int:pk>/add_ingredient/', views.add_ingredient, name='add_ingredient'),
+    path('accounts/signup', views.sign_up, name='sign_up')
+]
